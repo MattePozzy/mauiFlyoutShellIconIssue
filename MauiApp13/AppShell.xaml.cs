02961237a745c97b4d4a8368f6032f096e81b3e1
Matteo <7420345+MattePozzy@users.noreply.github.com>
2024-06-10 08:49:26 +0200

    Add files via upload

---
+++ MauiApp13/AppShell.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MauiApp13
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        public static void ChangeHamburger()
+        {
+            Color color = Colors.Red;
+            Shell.Current.FlyoutIcon = IconaMenu(MaterialFontIcons.HamburgerCheck, color);
+        }
+
+
+        public static void UpdateIconMenu(string title, string materialIcon, Color color)
+        {
+            int indx = Shell.Current.Items.ToList().FindIndex(x => string.Equals(x.Title, title, StringComparison.InvariantCultureIgnoreCase));
+            if (indx > -1)
+            {
+                Shell.Current.Items[indx].FlyoutIcon = IconaMenu(materialIcon, color);
+            }
+        }
+
+        public static FontImageSource IconaMenu(string icona, Color colore)
+        {
+            return new FontImageSource()
+            {
+                Glyph = icona,
+                FontAutoScalingEnabled = true,
+                FontFamily = "FontIcone",
+                Color = colore,
+                Size = 30
+            };
+        }
+
+    }
+}
---
+++ MauiApp13/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace MauiApp13
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+
+            // Try to change the home menu's voice.
+            string menuToFind = "Home";
+            AppShell.UpdateIconMenu(menuToFind, MaterialFontIcons.Abacus, Colors.Orange);
+
+            // try to change the hamburger top icon.
+            AppShell.ChangeHamburger();
+
+            DisplayAlert("info", "methods called, flyout menu and 'home' icons and colors should be changed.", "ok");
+        }
+    }
+
+}
